6bb2bdf76eec12025948a6672dab5346803a0bbc
kanishksinghal74-beep <kanishksinghal74@gmail.com>
2026-09-25 07:10:55 +0530

    Add device parameter to sentiment analysis pipeline

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-classifier = pipeline( "sentiment-analysis",  model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+classifier = pipeline( "sentiment-analysis",  model="distilbert/distilbert-base-uncased-finetuned-sst-2-english",device=-1)
 
 def predict_sentiment(text:str):    
 	result = classifier(text)[0]    
